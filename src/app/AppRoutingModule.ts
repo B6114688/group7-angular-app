5d4bc7f5426f17f42c1f42a3b174aef0654af01e
‘TonThanawatN’ <ton254365@gmail.com>
2022-11-08 11:58:12 +0700

    update addbook

--- src/app/AppRoutingModule.ts
+++ src/app/AppRoutingModule.ts
@@ -3,6 +3,7 @@ import { BasketComponent } from './components/basket/basket.component';
 import { NgModule } from '@angular/core' 
 import { Routes , RouterModule } from '@angular/router'
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { AddbookComponent } from './components/addbook/addbook.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
@@ -12,6 +13,7 @@ const routes:Routes=[
     {path:'register',component:RegisterComponent},
     {path:'home', component: HomepageComponent},
     {path:'books', component: BooksComponent},
+    {path:'addbook', component: AddbookComponent},
     {path:'basket', component: BasketComponent}
 ];
 
